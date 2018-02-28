ec6c83fd1c9010d8b90b1d7c8fd1d8d28c3f98ca
Jonathan Gall <odajay@gmail.com>
2018-02-28 21:50:46 +0100

    Flake 8 review

--- tinkering/flask_trials/app.py
+++ tinkering/flask_trials/app.py
@@ -1,16 +1,17 @@
-from flask import Flask
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, flash, render_template, request, session
 import os
- 
+
 app = Flask(__name__)
- 
+
+
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
         return "Hello Boss!"
- 
+
+
 @app.route('/login', methods=['POST'])
 def do_admin_login():
     if request.form['password'] == 'password' and request.form['username'] == 'admin':
@@ -18,7 +19,8 @@ def do_admin_login():
     else:
         flash('wrong password!')
     return home()
- 
+
+
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
     app.run(debug=True,host='0.0.0.0', port=4000)
