0c2c0213cf468b71b94bcdce22927fc403e0995b
Jonathan Gall <odajay@gmail.com>
2018-03-03 04:57:53 +0100

    Updating Login file to include CSS

--- tinkering/flask_trials/app.py
+++ tinkering/flask_trials/app.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def do_admin_login():
-    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+    if (request.form['password'] == 'password' and request.form['username'] == 'admin'):
         session['logged_in'] = True
     else:
         flash('wrong password!')
