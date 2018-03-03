ff99fddcb2593827b11656fc12044347591f88c5
Jonathan Gall <odajay@gmail.com>
2018-03-03 05:13:19 +0100

    Flask8

--- tinkering/flask_trials/app.py
+++ tinkering/flask_trials/app.py
@@ -20,10 +20,12 @@ def do_admin_login():
         flash('wrong password!')
     return home()
 
+
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
     return home()
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
