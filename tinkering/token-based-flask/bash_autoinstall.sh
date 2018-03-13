e1781436553d47861271179c986fab4f66ab598c
Jonathan Gall <odajay@gmail.com>
2018-03-13 20:43:14 +0100

    Realizing I'm a dumbass as all was due to my lack of waiting time in my script

--- tinkering/token-based-flask/bash_autoinstall.sh
+++ tinkering/token-based-flask/bash_autoinstall.sh
@@ -14,15 +14,31 @@ echo "Creating all the folders"
 # mkdir redis_data && mkdir postgres_data && mkdir nginx_data
 #root@pophubserver:/mydata# mkdir log_mynodeapp && mkdir log_nginx
 
-echo "Starting PostgreSQL Docker Session, create a user and password, naming it postgres, and mapping local drive /data/postgres_data"
-docker run -d --name postgres -e POSTGRES_PASSWORD=postgres -v $BASEDIR/data/postgres_data:/var/lib/postgresql/data --restart=unless-stopped postgres
+echo "Running PostgreSQL, creating a user and password, naming it postgres, and mapping local drive /data/postgres_data. We call the official image which has by default the port 5432 exposed. The default postgres user and database are created in the entrypoint with initdb"
+#docker run  --name postgres -e POSTGRES_PASSWORD=postgres -v $BASEDIR/data/postgres_data:/var/lib/postgresql/data --restart=unless-stopped -d postgres
 
-# docker exec -i -t postgres bash
+sudo docker run -d --name postgres_db postgres:9.6
 
-docker exec -it postgres psql -U postgres -W postgres <<EOF
-CREATE DATABASE flask_jwt_auth;
-CREATE DATABASE flask_jwt_auth_test; 
-EOF
+# to simply execute bash
+# sudo docker exec -it postgres_db /bin/bash 
+sleep 10  # Waits 5 seconds
+# to simply execute bash
+sudo docker exec -it postgres_db psql -U postgres 
+
+
+# docker exec -it postgres su postgres psql
+
+# docker exec -tiu postgres postgres psql
+
+# docker exec -tiu postgres psql -U postgres
+
+
+# docker exec -it postgres psql -U postgres -W postgres postgres
+
+# docker exec postgres psql -U postgres <<EOSQL
+# CREATE DATABASE flask_jwt_auth;
+# CREATE DATABASE flask_jwt_auth_test; 
+# EOSQL
 
 
 #docker stop $(docker ps -a -q) && docker rm -f $(docker ps -a -q) && rm -rf /Users/jay/Code/Docker/Dockerfiles/tinkering/token-based-flask/data
