6d551e6081e69d6f37582e24eb4a215715ca3057
Jonathan Gall <odajay@gmail.com>
2018-03-14 17:12:25 +0100

    Adding import clauses on the database.py file

--- tutorials/ameya_nginx_flask_postgres/database.py
+++ tutorials/ameya_nginx_flask_postgres/database.py
@@ -1,17 +1,26 @@
 # These env variables are the same ones uesd for the DB container
 
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
 user = os.environ['POSTGRES_USER']
 pwd = os.environ['POSTGRES_PASSWORD']
 db = os.environ['POSTGRES_DB']
-host = 'db'  # docker-compose creates a hostname alias with the service name
-port - '5432'  # default postgres port
-engine = create_engine('postgres://%s:%s@%s:%s/%s' % (user, pwd, host, port, db))
+host = 'db'
+port = '5432'
+engine = create_engine('postgres://%s:%s@%s:%s/%s' % (user, pwd, host, port, db)) 
 
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=engine))
 Base = declarative_base()
+Base.query = db_session.query_property()
 
 def init_db():
-    # import all modules here that might define models so that 
-    # they will be registerd properly on the metadata. Otherwise 
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
     import models
     Base.metadata.create_all(bind=engine)
