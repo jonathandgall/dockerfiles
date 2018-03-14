b2bd93422fe78c8677432270aec54ff1feb16fd9
Jonathan Gall <odajay@gmail.com>
2018-03-14 07:28:30 +0100

    Adding an await function to wait until the PostgreSQL is fully running

--- tinkering/token-based-flask/bash_autoinstall.sh
+++ tinkering/token-based-flask/bash_autoinstall.sh
@@ -3,10 +3,12 @@
 clear
 
 
-echo "Script installing the Dockerfiles for an automatic setup of our Nakara system"
+echo "Script installing the Dockerfiles for an automatic setup of our Nakara system
+"
 
 echo "Setting up parameters"
 BASEDIR="$PWD"
+POSTGRES_CONTAINER="docker run -d --name postgres postgres:9.6"
 
 
 echo "Creating all the folders"
@@ -14,32 +16,26 @@ echo "Creating all the folders"
 # mkdir redis_data && mkdir postgres_data && mkdir nginx_data
 #root@pophubserver:/mydata# mkdir log_mynodeapp && mkdir log_nginx
 
-echo "Running PostgreSQL, creating a user and password, naming it postgres, and mapping local drive /data/postgres_data. We call the official image which has by default the port 5432 exposed. The default postgres user and database are created in the entrypoint with initdb"
-#docker run  --name postgres -e POSTGRES_PASSWORD=postgres -v $BASEDIR/data/postgres_data:/var/lib/postgresql/data --restart=unless-stopped -d postgres
+echo "
+.: FIRST STEP :.
+> Running PostgreSQL container
+> Naming it postgres_db
+> mapping local drive /data/postgres_data
+> Creating a user and password
+> Official image: port 5432 exposed. Default postgres user and database are created in the entrypoint with initdb"
+docker run  --name postgres_db -e POSTGRES_PASSWORD=postgres_pass -v $BASEDIR/data/postgres_data:/var/lib/postgresql/data --restart=unless-stopped -d postgres
 
-sudo docker run -d --name postgres_db postgres:9.6
+IP=$(docker inspect -f '{{ .NetworkSettings.IPAddress }}' postgres_db)
 
-# to simply execute bash
-# sudo docker exec -it postgres_db /bin/bash 
-sleep 10  # Waits 5 seconds
-# to simply execute bash
-sudo docker exec -it postgres_db psql -U postgres 
+# wait for the database to come up
+until docker exec postgres_db psql -U postgres; do
+    echo "Waiting for postgres container..."
+    sleep 2
+done
 
+# create databases
 
-# docker exec -it postgres su postgres psql
+docker exec postgres_db psql -U postgres -c "CREATE DATABASE flask_jwt_auth"
+docker exec postgres_db psql -U postgres -c "CREATE DATABASE flask_jwt_auth_test"
 
-# docker exec -tiu postgres postgres psql
-
-# docker exec -tiu postgres psql -U postgres
-
-
-# docker exec -it postgres psql -U postgres -W postgres postgres
-
-# docker exec postgres psql -U postgres <<EOSQL
-# CREATE DATABASE flask_jwt_auth;
-# CREATE DATABASE flask_jwt_auth_test; 
-# EOSQL
-
-
-#docker stop $(docker ps -a -q) && docker rm -f $(docker ps -a -q) && rm -rf /Users/jay/Code/Docker/Dockerfiles/tinkering/token-based-flask/data
 
